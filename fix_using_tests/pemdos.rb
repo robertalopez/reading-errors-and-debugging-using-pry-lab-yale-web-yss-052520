5f4954fe0b35b5f44a197cff030c500c0a8ae047
Robert Lopez <robert.lopez@yale.edu>
2020-05-12 21:11:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,8 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-     
-      ssstring = "ssssssssss" + string
-      ssstring 
-    end 
+     ssstring = "ssssssssss" + string
+     ssstring 
   else
     string
   end
